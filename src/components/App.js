436f565329095396ca9fe46f75c68142481a2728
Daniel Hogan <hogan.daniel@hotmail.com>
2020-04-28 19:16:31 -0700

    Render memorygame in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import MemoryGame from '../containers/MemoryGame/MemoryGame';
 
 export default function App() {
   return <div>
-     
+     <MemoryGame />
   </div>;
 }
-  
